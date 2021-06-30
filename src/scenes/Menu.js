3c6b5467f2e68ea08e828ba4ea2207c0bce2e2da
Hazim Awad <haz.awad4@gmail.com>
2021-06-30 09:56:03 -0700

    changed menu UI

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -3,9 +3,6 @@ class Menu extends Phaser.Scene {
         super("menuScene");
     }
 
-    init() {
-
-    }
     preload() {
         // load audio
         this.load.audio('sfx_select', './assets/mixkit-game-ball-tap-2073.wav');
@@ -21,19 +18,19 @@ class Menu extends Phaser.Scene {
         let menuConfig = {
             fontFamily: 'Courier',
             fontSize: '28px',
-            backgroundColor: '#F3B141',
-            color: '#843605',
+            backgroundColor: '#ffdef2',
+            color: '#000',
             align: 'right',
             padding: {
                 top: 5, bottom: 5
             },
             fixedWidth: 0
         }
-        this.add.text(game.config.width/2, game.config.height/2 - (borderUISize + borderPadding), 'CAKE DESIGN', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire ', menuConfig).setOrigin(0.5);
-        menuConfig.backgroundColor = "#00FF00";
-        menuConfig.color = "#000";
-        this.add.text(game.config.width/2, game.config.height/2 + (borderUISize + borderPadding),'Press ← for Novice or → Expert', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/1.65, game.config.height/1.45 - (borderUISize + borderPadding), 'CAKE DESIGN', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/1.65, game.config.height/1.4, 'Use ←→ arrows to', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/1.65, game.config.height/1.25, 'move & (F) to fire ', menuConfig).setOrigin(0.5);
+        menuConfig.backgroundColor = "#ffffe3";
+        this.add.text(game.config.width/1.8, game.config.height/1.2 + (borderUISize + borderPadding),'Press ← for Novice or → Expert', menuConfig).setOrigin(0.5);
 
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
